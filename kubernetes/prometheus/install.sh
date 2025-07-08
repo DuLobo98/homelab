edfb84f4f702a5dcb82facb56f2757b1fe12d203
Duarte Lobo <duartem98@hotmail.com>
2025-07-08 22:48:07 +0100

    feat(k8s): add kube-prometheus-stack

---
+++ kubernetes/prometheus/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install kube-prometheus-stack \
+  prometheus-community/kube-prometheus-stack \
+  -n monitoring \
+  --create-namespace \
+  -f values.yaml
+
+  #prom-operator grafana password
