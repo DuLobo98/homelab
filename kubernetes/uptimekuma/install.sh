3f9e6cffa3b2e1b07a22d7e9472501640a46bad7
Duarte Lobo <duartem98@hotmail.com>
2025-11-27 21:59:26 +0000

    feat(k8s): remove obsolete configuration files and installation scripts

--- kubernetes/uptimekuma/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-helm repo add uptime-kuma https://dirsigler.github.io/uptime-kuma-helm
-helm repo update
-helm install uptime-kuma \
-    uptime-kuma/uptime-kuma \
-    -n uptime-kuma \
-    --create-namespace \
-    -f values.yaml
